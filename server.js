fc45df5b4315f705491c89b75c0880da5255ba48
Lucas Wiener <lucas@wiener.se>
2013-05-12 20:53:08 +0200

    Installed express, nconf, winston. Init config, and logger

---
+++ server.js
@@ -0,0 +1,9 @@
+//Used for handling logging.
+var logger = require('./lib/log');
+
+//Used to get info about the component.
+var info = require('./lib/info');
+
+logger.log('info', 'Started %s component with ID %d', info.getName(), info.getID());
+
+logger.log('info', 'Terminated %s component with ID %d', info.getName(), info.getID());
