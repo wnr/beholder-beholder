47124369ee55e05c1a209103c7d86f3e0965f752
Lucas Wiener <lucas@wiener.se>
2013-05-23 21:39:17 +0200

    Added test dir and empty test js file for config

---
+++ test/test.js
@@ -0,0 +1,16 @@
+//Use the expect.js style of asserting conditions.
+var expect = require('expect.js');
+
+//Startup config tests.
+describe('config', function() {
+
+	//Tests regarding Log configs
+	describe('log', function() {
+		
+	});
+
+	//Tests regarding API configs
+	describe('api', function() {
+
+	});
+});
