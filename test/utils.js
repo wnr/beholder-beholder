04bbe48ffbb011a503b82928a5177efc52b32394
Lucas Wiener <lucas@wiener.se>
2013-06-12 16:09:29 +0200

    Update config_defaults.json to new logging system

--- test/utils.js
+++ test/utils.js
@@ -78,4 +78,112 @@ describe('utils', function() {
 			expect(function() { utils.validate(1337, undefined, 'a string'); }).to.throwError();
 		});
 	});
+
+	describe('validators', function() {
+		it('should be an object', function() {
+			//The 'validators' object should be an object.
+			expect(utils.validators).to.be.an('object');
+		});
+
+		describe('type', function() {
+			it('should be a function', function() {
+				//The type should be a function.
+				expect(utils.validators.type).to.be.a('function');
+			});
+
+			it('should return a function', function() {
+				//The function should return a validator function.
+				expect(utils.validators.type('something')).to.be.a('function');
+			});
+
+			it('should validate right', function() {
+				//Create a function that validates values of type 'number'.
+				var numberValidator = utils.validators.type('number');
+
+				//The validator function should return 'true' when given an actual number of type 'number'.
+				expect(numberValidator(404)).to.equal(true);
+
+				//The validator function should return 'false' when given an actual number of other type than 'number'.
+				expect(numberValidator(true)).to.equal(false);
+			});
+
+			it('should throw error on invalid type parameter', function() {
+				//The function returned should throw an error with parameter 'type' != string.
+				expect(function() { utils.validators.type(1337); }).to.throwError();
+			});
+		});
+
+		describe('typeOrFalse', function() {
+			it('should be a function', function() {
+				//The typeOrFalse should be a function.
+				expect(utils.validators.typeOrFalse).to.be.a('function');
+			});
+
+			it('should return a function', function() {
+				//The function should return a validator function.
+				expect(utils.validators.typeOrFalse('something')).to.be.a('function');
+			});
+
+			it('should validate right', function() {
+				//Create a function that validates values of type 'number'.
+				var numberValidator = utils.validators.typeOrFalse('number');
+
+				//The validator function should return 'true' when given an actual number of type 'number'.
+				expect(numberValidator(404)).to.equal(true);
+
+				//The validator function should return 'false' when given an actual number of other type than 'number'.
+				expect(numberValidator(true)).to.equal(false);
+
+				//Since the value can be 'false' the validator should return 'true' when it is.
+				expect(numberValidator(false)).to.equal(true);
+			});
+
+			it('should throw error on invalid typeOrFalse parameter', function() {
+				//The function returned should throw an error with parameter 'type' != string.
+				expect(function() { utils.validators.typeOrFalse(1337); }).to.throwError();
+			});
+		});
+
+		describe('falseOrObject', function() {
+			it('should be a function', function() {
+				//The falseOrObject should be a function.
+				expect(utils.validators.falseOrObject).to.be.a('function');
+			});
+
+			it('should return a function', function() {
+				//The function should return a validator function.
+				expect(utils.validators.falseOrObject('something')).to.be.a('function');
+			});
+
+			it('should validate right', function() {
+				//Create an object with three properties to validate.
+				var object = {
+					foo: 'function',
+					bar: 'string',
+					bird: 'object'
+				};
+
+				//Create a function that validates values objects that can be false with the defined properties.
+				var objectValidator = utils.validators.falseOrObject(object);
+
+				//The validator function should return 'true' when given an actual value 'false'.
+				expect(objectValidator(false)).to.equal(true);
+
+				//The validator function should return 'false' when given something else than 'false' or 'object'.
+				expect(objectValidator(true)).to.equal(false);
+
+				//The validator function should return 'false' when given a object with the wrong properties.
+				expect(objectValidator({ another: 'object' })).to.equal(false);
+
+				//The validator function should return 'false' if the properties are defined but with wrong types.
+				expect(objectValidator({ foo: 1337, bar: function() {}, bird: 'lol' })).to.equal(false);
+
+				//The validator function should return 'true' when the properties are defined and have right types.
+				expect(objectValidator({ foo: function() {}, bar: 'hello', bird: {} })).to.equal(true);
+			});
+
+			it('should throw error on invalid falseOrObject parameter', function() {
+			});
+		});
+	});
 });
